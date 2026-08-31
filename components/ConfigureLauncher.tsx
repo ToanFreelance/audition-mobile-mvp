441472f8598ce4625572ae076678d24bb1519bb0
ToanFreelance <80197261+ToanFreelance@users.noreply.github.com>
2026-08-31 20:30:05 +0700

    feat: add ready screen configure launcher

---
+++ components/ConfigureLauncher.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ConfigureLauncher() {
+  return (
+    <a className="configure-launcher" href="/tools/music-config">
+      ⚙ CONFIGURE MUSIC
+    </a>
+  );
+}
