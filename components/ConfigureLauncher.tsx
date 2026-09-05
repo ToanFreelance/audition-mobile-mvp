a27c196cb76c25a5aac442e1ae308de970236a42
ToanFreelance <80197261+ToanFreelance@users.noreply.github.com>
2026-09-05 21:41:57 +0700

    Add audio timing lab shortcut to ready page

--- components/ConfigureLauncher.tsx
+++ components/ConfigureLauncher.tsx
@@ -2,8 +2,13 @@
 
 export default function ConfigureLauncher() {
   return (
-    <a className="configure-launcher" href="/tools/music-config">
-      ⚙ CONFIGURE MUSIC
-    </a>
+    <div style={{ display: "flex", gap: 8, position: "fixed", right: 12, bottom: 12, zIndex: 1000 }}>
+      <a className="configure-launcher" href="/tools/audio-timing" style={{ position: "static" }}>
+        🧪 AUDIO TIMING
+      </a>
+      <a className="configure-launcher" href="/tools/music-config" style={{ position: "static" }}>
+        ⚙ CONFIGURE MUSIC
+      </a>
+    </div>
   );
 }
