25e00c109385ca189094caa93d672a034c9349b1
ToanFreelance <80197261+ToanFreelance@users.noreply.github.com>
2026-09-12 19:02:02 +0700

    chore(ui): load judgement feedback skin

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <link rel="stylesheet" href="/audition-figma.css" />
         <link rel="stylesheet" href="/audition-final-overrides.css" />
         <link rel="stylesheet" href="/audition-gauge.css" />
+        <link rel="stylesheet" href="/audition-judgement.css" />
         <link rel="stylesheet" href="/music-config-overrides.css" />
         <link rel="stylesheet" href="/song-picker.css" />
         <link rel="stylesheet" href="/music-config-v3.css" />
